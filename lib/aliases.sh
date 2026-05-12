531304deb5d709d55746aa78138f981bbb4b976d
lelackwarren-ui <lelackwarren@gmail.com>
2026-05-13 02:00:44 +0400

    Add new aliases

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -10,3 +10,14 @@ alias cpuinfo='lscpu | grep -E "Model name|CPU\(s\)|MHz"'
 alias ports='ss -tulnp'
 # Sécurité
 alias rm='rm -i' # demande toujours confirmation avant suppression
+#Droit
+alias chx="chmod +x"
+alias chw="chmod +w"
+alias chr="chmod +r"
+#Dosssier
+alias mkr="mkdir"
+alias th="touch"
+#mettre a jour
+alias saud="sudo apt update"
+alias saug="sudo apt upgrade"
+alias sain="sudo apt install"
